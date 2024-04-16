abebb26e022a2f7596424222ec4eb91c9ce7edd3
andreas-unleash <andreas@getunleash.ai>
2024-04-16 10:53:10 +0300

    format

--- lib/parse_stringify_toggles.dart
+++ lib/parse_stringify_toggles.dart
@@ -5,23 +5,12 @@ import 'package:unleash_proxy_client_flutter/toggle_config.dart';
 /// Parses toggles from the server or local storage so that they can be used
 /// as structured data
 Map<String, ToggleConfig> parseToggles(String body) {
-  var decoded = jsonDecode(body);
-
-  // Return an empty map if 'toggles' isn't a list or doesn't exist
-  if (decoded['toggles'] is! List) {
-    return {};
-  }
-
-  var toggles = decoded['toggles'] as List;
-  var result = <String, ToggleConfig>{};
-
-  for (var toggle in toggles) {
-    if (toggle is Map<String, dynamic> && toggle.containsKey('name')) {
-      result[toggle['name']] = ToggleConfig.fromJson(toggle);
-    }
-  }
-
-  return result;
+  final data = jsonDecode(body)['toggles'];
+  // Check if there is anything to map over? Otherwise map might cause an error
+  // Write a test that checks if the
+  return {
+    for (var toggle in data) toggle['name']: ToggleConfig.fromJson(toggle)
+  };
 }
 
 Map<String, dynamic> toJSON(String toggleName, ToggleConfig toggle) {
